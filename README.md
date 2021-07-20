c99622ae1ec1f82c5585528f845ab83c6db7b77c
Li Xueli <mixj93@163.com>
2021-07-20 15:42:26 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rust-by-example
+
+https://rustwiki.org/zh-CN/rust-by-example/index.html
--- hello.rs
+++ hello.rs
@@ -6,4 +6,13 @@
 fn main() {
   // println! 是一个宏 macros
   println!("Hello World");
+
+  println!("{} days", 31); // 31默认是i32类型
+  println!("{} days", 31i64);
+
+  println!("{0}, this is {1}. {1}, this is {0}.", "Alice", "Bob");
+
+  println!("{subject} {verb} {object}", object="the lazy dog", subject="the quick brown fox", verb="jump over");
+
+  println!("{} of {:b} people know binary, the other half don't", 1, 2);
 }
